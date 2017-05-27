3359fcd7151c0c9b38d6c4a2165bec17fad09c07
math-miki <math.micky.dtl@gmail.com>
2017-05-27 23:16:42 +0900

    first commit

---
+++ integrateImages/CanvasImageView.swift
@@ -0,0 +1,56 @@
+//
+//  canvasImage.swift
+//  integrateImages
+//
+//  Created by 高橋幹 on 2017/05/27.
+//  Copyright © 2017年 高橋幹. All rights reserved.
+//
+
+import UIKit
+
+class CanvasImageView: UIView {
+    var pathes: [(start: CGPoint,fin: CGPoint)] = []
+    var startPoint: CGPoint!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("start")
+        let touch = touches.first
+        startPoint = touch?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        let touch = touches.first
+        pathes.append((start: startPoint,fin: (touch?.location(in: self))!))
+        startPoint = touch?.location(in: self)
+        
+        self.setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+            let context = UIGraphicsGetCurrentContext()
+            context!.beginPath()
+            
+            for path in pathes {
+                context?.move(to: path.start)
+                context?.addLine(to: path.fin)
+            }
+            
+        context!.setStrokeColor(red: 1, green: 0, blue: 0, alpha: 1)
+        context!.setLineWidth(1)
+        context!.setLineCap(.round)
+        context!.strokePath()
+    }
+    
+    
+}
+//    
+//    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+//        var newPoint = (touches.anyObject() as AnyObject).location(self)
+//        pathes.append(Line(start: startPoint, end: newPoint!))
+//        startPoint = newPoint
+//        
+//        self.setNeedsDisplay()
+//        
+//    }
+
---
+++ integrateImages/PaintViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PaintViewController.swift
+//  integrateImages
+//
+//  Created by 高橋幹 on 2017/05/27.
+//  Copyright © 2017年 高橋幹. All rights reserved.
+//
+
+import UIKit
+
+class PaintViewController: UIViewController {
+
+    @IBOutlet weak var canvasView: CanvasImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveAsImage(_ sender: Any) {
+        
+        UIGraphicsBeginImageContextWithOptions(canvasView.frame.size, true, 0)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: 0,y: 0)
+        canvasView.layer.render(in: context)
+        let renderedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(renderedImage, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    func showResultOfSaveImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        
+        var title = "保存完了"
+        var message = "カメラロールに保存しました"
+        
+        if error != nil {
+            title = "エラー"
+            message = "保存に失敗しました"
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // OKボタンを追加
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // UIAlertController を表示
+        self.present(alert, animated: true, completion: nil)
+    }
+}
